8872fdcee1fac8351463f610d2b85cd0392944e0
indraniel <indraniel@gmail.com>
2016-11-23 18:08:22 -0600

    + first-pass with clojure.tools.cli
      - still feels ugly to me...

--- src/clojure_example_cli/core.clj
+++ src/clojure_example_cli/core.clj
@@ -1,8 +1,9 @@
 (ns clojure-example-cli.core
   (:require [clojure.tools.cli :as cli]
-            [clj-sub-command.core :as sc]
             [clojure.string :as string])
   (:gen-class))
+
+(def prg-name "foo")
 
 (defn error-msg [errors]
   (str "The following errors occurred while parsing your command:\n\n"
@@ -12,34 +13,41 @@
   (println msg)
   (System/exit status))
 
+(defn missing-required-opts?
+  [required-opts opts]
+  (if (empty? required-opts)
+    false
+    (not-every? opts required-opts)))
+
+(defn cmd-usage [usage-string options-summary]
+  (->> [usage-string
+        ""
+        "Options:"
+        options-summary]
+       (string/join "\n")))
+
+
 ;; "cmd1" command
 
 (def cmd1-options
   [["-v" "--verbose"]
    ["-h" "--help"]])
 
-;;(def required-cmd1-opts #{})
+(def cmd1-required-opts #{})
 
-(defn cmd1-usage [options-summary]
-  (->> ["Usage: foo cmd1 [options] file"
-        ""
-        "Options:"
-        options-summary]
-       (string/join "\n")))
-
-; (defn missing-required-cmd1-opts?
-;  [opts]
-;  (not-every? opts required-cmd1-opts))
+(def cmd1-usage-str (str "Usage: " prg-name " cmd1 [options]"))
 
 (defn cmd1 [args]
   (let [{:keys [options arguments errors summary]} (cli/parse-opts args cmd1-options)]
-    (println options)
-    (println arguments)
     (cond
-      (:help options) (do (println "Foo") (exit 0 (cmd1-usage summary))) 
- ;;     (missing-required-cmd1-opts? options) (cmd1-usage summary)
-      ((complement empty?) arguments) (do (println "Baz") (exit 1 (cmd1-usage summary)))
+      (:help options)
+        (do (println "Foo") (exit 0 (cmd-usage cmd1-usage-str summary))) 
+      (missing-required-opts? cmd1-required-opts options)
+        (cmd-usage cmd1-usage-str summary)
+      ((complement empty?) arguments)
+        (do (println "Baz") (exit 1 (cmd-usage cmd1-usage-str summary)))
       errors (exit 1 (error-msg errors)))
+
     (if (:verbose options)
       (println "In verbose mode"))))
 
@@ -51,42 +59,50 @@
     :parse-fn #(identity %)]
    ["-h" "--help"]])
 
-(def required-cmd2-opts #{:file})
+(def cmd2-required-opts #{:file})
 
-(defn cmd2-usage [options-summary]
-  (->> ["Usage: foo cmd2 [options] file"
-        ""
-        "Options:"
-        options-summary]
-       (string/join "\n")))
-
-(defn missing-required-cmd2-opts?
-  [opts]
-  (not-every? opts required-cmd2-opts))
+(def cmd2-usage-str (str "Usage: " prg-name " cmd2 [options] file"))
 
 (defn cmd2 [args]
   (let [{:keys [options arguments errors summary]} (cli/parse-opts args cmd2-options)]
+
     (cond
-      (:help options) (exit 0 (cmd2-usage summary))
-      (missing-required-cmd2-opts? options) (cmd2-usage summary)
-      ((complement empty?) arguments) (exit 1 (cmd2-usage summary))
+      (:help options)
+        (do (println "Foo") (exit 0 (cmd-usage cmd2-usage-str summary))) 
+      (missing-required-opts? cmd2-required-opts options)
+        (cmd-usage cmd2-usage-str summary)
+      ((complement empty?) arguments)
+        (do (println "Baz") (exit 1 (cmd-usage cmd2-usage-str summary)))
       errors (exit 1 (error-msg errors)))
+
     (if (:verbose options)
       (println "In verbose mode"))
+
     (if (:file options)
       (println "Got file" (:file options)))))
 
+(def master-cmd-options
+  [["-h" "--help" (str "Overall help for " prg-name)]])
+
+(defn master-cmd-usage [options-summary]
+  (->> ["This is my awesome program."
+        ""
+        (str "Usage: " prg-name " [action] [options]")
+        "Options:"
+        options-summary
+        ""
+        "Actions:"
+        "    cmd1    cmd1 short description"
+        "    cmd2    cmd2 short description"]
+       (string/join "\n")))
+
 (defn -main [& args]
-  (let [[opts cmd args help cands]
-        (sc/sub-command args
-                        "Usage: foo [-h] {cmd1,cmd2} ..."
-                        :options [["-h" "--help" "Show help" :default false :flag true]]
-                        :commands [["cmd1" "Description for cmd1"]
-                                   ["cmd2" "Description for cmd2"]])]
-    (when (:help opts)
-      (exit 0 help))
-    (case cmd
-      :cmd1 (cmd1 args)
-      :cmd2 (cmd2 args)
-      (exit 1 (str "Invalid command.  See 'foo --help'.\n\n"
-                   (sc/candidate-message cands))))))
+  (let [{:keys [options arguments errors summary]} (cli/parse-opts args master-cmd-options :in-order true)]
+    (cond
+      (:help options) (exit 0 (master-cmd-usage summary))
+      (not (>= (count arguments) 1))  (exit 1 (master-cmd-usage summary))
+      errors (exit 1 (error-msg errors)))
+    (case (keyword (first arguments))
+      :cmd1 (cmd1 (rest arguments))
+      :cmd2 (cmd2 (rest arguments))
+      (exit 1 (str "Invalid command (" (first arguments) ")  See '" prg-name " --help'.")))))
